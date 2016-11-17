ca2289e9d87739a71f0fd61da4b60e8b88ae0c9f
Nikita Bogdan <nikita.bogdan@me.com>
2016-11-17 14:15:44 +0000

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/nikitabogdan/top-comp-com.git'
 
 set :deploy_to, '/home/deploy/top-comp-com'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{'config/database.yml', 'config/secrets.yml'}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
