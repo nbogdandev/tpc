0ce7c5fcc76563ea1b4cd07df8801e62425f834f
nikitabogdan <nikita.bogdan@me.com>
2017-03-06 19:36:31 +1000

    add text

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.7.1'
+lock '3.7.2'
 
 set :application, 'top-comp-com'
 set :repo_url, 'https://github.com/nikitabogdan/top-comp-com.git'
